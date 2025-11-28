c761bd6b91447a20b0f25e9fc8105812a3fd7549
tiennm99 <tiennm99@outlook.com>
2025-11-29 00:43:53 +0700

    Update hhcn.cpp

--- cpp/hhcn.cpp
+++ cpp/hhcn.cpp
@@ -4,7 +4,10 @@ using namespace std;
 int main() {
     long long a, b, c;
     cin >> a >> b >> c;
+    long long x = sqrt(a * b / c);
+    long long y = sqrt(b * c / a);
+    long long z = sqrt(c * a / b);
 
-    cout << 4 * (a * b + b * c + c * a) / sqrt(a * b * c);
+    cout << 4 * (x + y + z);
     return 0;
 }
