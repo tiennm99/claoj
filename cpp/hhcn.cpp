255bcaef5f2528bd0bc8366eed30206745447f1b
tiennm99 <tiennm99@outlook.com>
2025-11-29 00:50:21 +0700

    feat(cpp): minify hhcn

--- cpp/hhcn.cpp
+++ cpp/hhcn.cpp
@@ -2,12 +2,9 @@
 using namespace std;
 
 int main() {
-    long long a, b, c;
+    double a, b, c;
     cin >> a >> b >> c;
-    long long x = sqrt(a * b / c);
-    long long y = sqrt(b * c / a);
-    long long z = sqrt(c * a / b);
 
-    cout << 4 * (x + y + z);
+    cout << 4 * (sqrt(a * b / c) + sqrt(b * c / a) + sqrt(c * a / b));
     return 0;
 }
